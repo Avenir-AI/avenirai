6a90f7217ec87e495ac8417d91b645bde14e535e
Ari Dulchinos <red178@hotmail.com>
2025-01-08 15:19:06 -0500

    added  contact pages

--- script.js
+++ script.js
@@ -32,3 +32,34 @@ document.addEventListener('DOMContentLoaded', () => {
         observer.observe(counter);
     });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    function loadHTML(url, id, callback) {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.onreadystatechange = function () {
+            if (this.readyState !== 4) return;
+            if (this.status !== 200) return;
+            document.getElementById(id).innerHTML = this.responseText;
+            if (callback) callback(); // Call the callback after loading the content
+        };
+        xhr.send();
+    }
+
+    loadHTML('/navigation.html', 'navigation-placeholder', function() {
+        // Navigation is fully loaded, now add event listeners
+        const hamburger = document.querySelector('.hamburger');
+        const nav = document.querySelector('header nav');
+        const body = document.body;
+
+        if (hamburger) {
+            hamburger.addEventListener('click', () => {
+                console.log("clicked button");
+                nav.parentElement.classList.toggle('nav-open');
+                body.classList.toggle('menu-open');
+            });
+        }
+    });
+
+    loadHTML('/footer.html', 'footer-placeholder');
+});
